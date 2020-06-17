ae628c70a661730b08cabd222fa4ee51bbbc661a
tao.xu <476189779@qq.com>
2020-06-17 17:19:48 +0800

    修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,11 +3,10 @@ import { MessageBox, Message } from 'element-ui';
 import store from '@/store';
 import { getToken } from '@/utils/auth';
 
-// create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API, 
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 60000 
 });
 
 // request interceptor
